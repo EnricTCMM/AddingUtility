8b7b8f424aa749039633c0139d55f9c43684513f
Enric Sesa Nogueras <sesa@edu.tecnocampus.cat>
2026-05-18 17:11:31 +0200

    Cohesion i GroupRegistry

---
+++ Assets/AICourse/MotorBehaviours/GroupRegistry.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Component: GroupRegistry
+ * Author: Dr. Enric Sesa i Nogueras
+ * Notice: This code contains generative AI contributions.
+ * Description: 
+ * Centralized registry to keep track of a group of agents (boids).
+ * Replaces expensive FindGameObjectsWithTag calls by providing a 
+ * maintained list of active members.
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MotorBehaviours
+{
+    public class GroupRegistry : MonoBehaviour
+    {
+        [Header("Registry Roster")]
+        [Tooltip("The list of boids registered in this group.")]
+        public List<GameObject> members = new List<GameObject>();
+
+        public void Awake()
+        {
+            AddInitialBoids();
+        }
+
+        // Subclasses should consider the possibility of overriding
+        // this method, especially if the group must not or should not
+        // add its children as members automatically.
+        public virtual void AddInitialBoids()
+        {
+            // Take all the objects parented by this GroupRegistry
+            // (i.e. the gameObject containing this component) 
+            // and make them members of the group
+            foreach (Transform child in transform)
+            {
+                AddBoid(child.gameObject);
+            }
+        }
+
+        public void AddBoid(GameObject boid)
+        {
+            if (!members.Contains(boid))
+            {
+                members.Add(boid);
+                
+                // Let the registry parent the boid to keep the hierarchy clean
+                boid.transform.parent = this.transform;
+            }
+        }
+
+        public void RemoveBoid(GameObject boid)
+        {
+            if (members.Contains(boid))
+            {
+                members.Remove(boid);
+                
+                // Unparent if it was parented to us
+                if (boid.transform.parent == this.transform)
+                {
+                    boid.transform.parent = null;
+                }
+            }
+        }
+        
+        // Convenience method for Steering Behaviours to retrieve the roster
+        public List<GameObject> GetMembers()
+        {
+            return members;
+        }
+    }
+}
---
+++ Assets/AICourse/MotorBehaviours/SB_Cohesion.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using Steerings; // Required for Utils.InCone
+
+namespace MotorBehaviours
+{
+    public class SB_Cohesion : LinearMotorBehaviour
+    {
+        [Header("Cohesion Settings")]
+        public string idTag = "Boid"; // Tag used to find neighbors
+        public float cohesionThreshold = 5f;
+        
+        [Header("Vision Settings")]
+        public bool applyVision = false;
+        public float coneOfVisionAngle = 90f;
+
+        // The method that is called by the MotorManager
+        public override Vector3? GetDesiredVelocity(MotorManager me)
+        {
+            // We pass the inspector variables to the static math method
+            return SB_Cohesion.GetDesiredVelocity(me, idTag, cohesionThreshold, applyVision, coneOfVisionAngle);
+        }
+
+        // Math is done in this method. Also available for external calls
+        public static Vector3? GetDesiredVelocity(MotorManager me, string targetTag, float threshold, bool useVision, float visionAngle)
+        {
+            // WARNING: FindGameObjectsWithTag is computationally expensive O(n^2).
+            // Ideal candidate to be replaced by a centralized BoidSensor later.
+            GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
+            
+            Vector3 centreOfMasses = Vector3.zero;
+            int mates = 0;
+
+            foreach (GameObject target in targets)
+            {
+                // Do not take yourself into account
+                if (target == me.gameObject)
+                {
+                    continue;
+                }
+
+                float distanceToTarget = (target.transform.position - me.transform.position).magnitude;
+
+                // Disregard distant targets
+                if (distanceToTarget > threshold) 
+                {
+                    continue;
+                }
+
+                // Disregard targets outside cone of vision if necessary
+                if (useVision)
+                {
+                    if (!Utils.InCone(me.gameObject, target, visionAngle)) 
+                    {
+                        continue;
+                    }
+                }
+
+                // Accumulate positions to calculate the center later
+                centreOfMasses += target.transform.position;
+                mates++;
+                
+            } // End of iteration over all cohesion targets 
+
+            // If there are no mates around, we abstain completely
+            if (mates == 0)
+            {
+                return null;
+            }
+
+            // Calculate the actual center of masses (average position)
+            centreOfMasses /= mates;
+
+            // EXPLICIT DELEGATION TO SEEK
+            // We mathematically delegate the actual steering calculation to SB_Seek
+            // asking it to go to the calculated center of masses.
+            return SB_Seek.GetDesiredVelocity(me, centreOfMasses);
+        }
+    }
+}
