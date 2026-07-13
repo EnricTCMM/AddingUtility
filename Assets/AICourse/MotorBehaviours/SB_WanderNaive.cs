56c0768d9c70ee63918e4311050f828999db8373
Enric Sesa i Nogueras <sesa@edu.tecnocampus.cat>
2026-07-13 19:18:57 +0200

    Encara una mica més

---
+++ Assets/AICourse/MotorBehaviours/SB_WanderNaive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MotorBehaviours
+{
+    public class SB_WanderNaive : LinearMotorBehaviour
+    {
+        [Header("Naive Wander Settings")]
+        [Tooltip("How much the agent can randomly turn per frame (in degrees)")]
+        public float wanderRate = 5f; 
+
+        public override Vector3? GetDesiredVelocity(MotorManager me)
+        {
+            // Slightly change agent's orientation
+            float angleChange = (Random.value - Random.value) * wanderRate;
+            
+            Vector3 currentRotation = me.transform.eulerAngles;
+            currentRotation.z += angleChange;
+            me.transform.eulerAngles = currentRotation;
+
+            // delegate
+            return SB_GoWhereYouLook.CalculateDesiredVelocity(me); 
+        }
+    }
+}
